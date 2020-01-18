c55d0dfd8cd0f936697cdb7790ece4cba320e491
anam soomro <anamsoomroed@gmail.com>
2020-01-18 02:09:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
-def welcome
-  # code #welcome here
+def welcome()
+  puts "Welcome to the Blackjack Table"
 end
 
-def deal_card
-  # code #deal_card here
+def deal_card()
+  random_number = 0
+  until !random_number == 0 do 
+    random_number = rand(11)
+  end
+  random_number
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to ${total}"
 end
 
-def prompt_user
-  # code #prompt_user here
+def prompt_user()
+  puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  # code #get_user_input here
+def get_user_input()
+  # gets idk how to use gets to be honest 
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+  puts "Sorry, you hit ${num}. Thanks for playing!"
 end
 
-def initial_round
-  # code #initial_round here
+def initial_round()
+  num = deal_card()
+  num += deal_card()
+  display_card_total(num)
+  num
 end
 
-def hit?
-  # code hit? here
+def hit?(current_total)
+  prompt_user()
+  next_play = get_user_input()
+  if next_play == 'h'
+    deal_card()
+  elsif next_play == 's'
+    end_game(current_total)
+  else 
+    invalid_command()
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
+  prompt_user()
 end
 
 #####################################################
